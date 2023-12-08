0a57564f928220d3747080bc4825d0125cc074ac
Arthur Gabriel <arthurgabrielmv@gmail.com>
2023-12-08 14:02:07 -0300

    initial commit  A Blog project in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MenuTitleComponent} from "./components/menu-title/menu-title.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
